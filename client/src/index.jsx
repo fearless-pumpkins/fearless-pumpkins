7eec363a4323a919ac952436a0edaef9b1b7cea4
Hack Reactor Student <student@hackreactor.com>
2017-08-09 11:34:53 -0700

    (feat) add input form for tests

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,8 +6,14 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
+      items: ['hello']
     };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
   }
 
   componentDidMount() {
@@ -20,6 +26,14 @@ class App extends React.Component {
       <div>
         <h1>Welcome to Tweetrics!</h1>
         <div>
+          <form className='mainForm'>
+            <h3>Enter a username</h3>
+            <input type="text" name="username"></input>
+            <button type="submit" onClick={this.handleClick}>SUBMIT</button>
+          </form>
+        </div>
+        <div>
+          <p>{this.state.items}</p>
         </div>
       </div>
     )
