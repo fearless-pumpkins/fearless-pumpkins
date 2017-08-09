2554915645d7b948518c3043bc3268738fa386ea
Yessengerey Bolatov <Yessengerey@users.noreply.github.com>
2017-08-09 12:26:29 -0700

    (feat) Feat/client Ajax request working and returning information (#17)
    * (doc) remove empty files
    
    * (doc) remove another empty file
    
    * (doc) Add empty css styles file for the future
    
    * (doc) Add react-dev script to run webpack watch
    
    * (feat) Start client ui implementation
    
    * (feat) add input form for tests
    
    * Removed unnecessary file
    
    * Add script command to run node server
    
    * Add post ajax request to LOCALHOST server for now

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,8 +6,31 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
+      friends: []
     };
+    this.url = 'http://localhost:3000';
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    var postObject = {
+      screenName: $('#inputUsername')[0].value
+    }
+
+    $.ajax({
+      type: 'POST',
+      url: this.url + '/friends/list',
+      data: JSON.stringify(postObject),
+      contentType: 'application/json',
+      success: (data) => {
+        console.log('POST request: success');
+        console.log(data);
+      },
+      error: (err) => {
+        console.log('POST request: error', err);
+      }
+    })
   }
 
   componentDidMount() {
@@ -18,7 +41,17 @@ class App extends React.Component {
 
     return (
       <div>
-        <p> Hello world! </p>
+        <h1>Welcome to Tweetrics!</h1>
+        <div>
+          <form className='mainForm'>
+            <h3>Enter a username</h3>
+            <input id="inputUsername" type="text" name="username"></input>
+            <button type="submit" onClick={this.handleClick}>SUBMIT</button>
+          </form>
+        </div>
+        <div>
+
+        </div>
       </div>
     )
   }
