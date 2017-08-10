51d7cac969ad2ab5c6f05c2e5ce4e8cf4b116d66
Hack Reactor Student <student@hackreactor.com>
2017-08-10 15:24:53 -0700

    (feat) Add flex to app

--- client/src/components/landing.jsx
+++ client/src/components/landing.jsx
@@ -5,15 +5,18 @@ import Feed from './feed.jsx';
 const Landing = (props) => {
   return (
     <div className="app">
-      <h1>Welcome to Tweetrics!</h1>
+
       <div className="mainForm">
+      <h1>Welcome to Tweetrics!</h1>
         <form>
           <h3>Enter a username</h3>
           <input id="inputUsername" type="text" name="username"></input>
           <button type="submit" onClick={props.handleClick}>SUBMIT</button>
         </form>
       </div>
+      
       <Feed feed={props.feed}/>
+
     </div>
   );
 };
