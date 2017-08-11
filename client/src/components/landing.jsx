a5f955e1d98827fc039f27bacd42882ec0da7b7c
Yessengerey Bolatov <Yessengerey@users.noreply.github.com>
2017-08-10 19:39:43 -0700

    Feat/ui Add a rough implementation of the components for the client side and their designs (#28)
    * (feat) Commence design of front end
    
    * (feat) Add client components and conditional rendering
    
    * Implement feed and landing components, add styling
    
    * (feat) Remove localhost from url, add another conditional to rendering
    
    * (feat) Add flex to app
    
    * (doc) Add ds store file to the list of gitignore files to prevent it from ever being tracked
    
    * (ui/ux) Implemented rough management and design of components

---
+++ client/src/components/landing.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Feed from './feed.jsx';
+
+
+const Landing = (props) => {
+  return (
+    <div className="app">
+
+      <div className="mainForm">
+      <h1>Welcome to Tweetrics!</h1>
+        <form>
+          <h3>Enter a username</h3>
+          <input id="inputUsername" type="text" name="username"></input>
+          <button type="submit" onClick={props.handleClick}>SUBMIT</button>
+        </form>
+      </div>
+      
+      <Feed feed={props.feed}/>
+
+    </div>
+  );
+};
+
+export default Landing;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,31 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import Landing from './components/landing.jsx';
+import Loading from './components/loading.jsx';
+import Analytics from './components/analytics.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      stage: 'landing',
+      analytics: {},
+      feed: {},
       friends: []
     };
-    this.url = 'http://localhost:3000';
     this.handleClick = this.handleClick.bind(this);
   }
 
+  // Handles the click event on the submit button
+  // When invoked, changes stage to loading until ajax request is successfully
+  // returned
   handleClick(event) {
+    // To prevent default auto refresh of the page
     event.preventDefault();
+
+    // Set stage to loading once button is clicked
+    this.setState({
+      stage: 'loading'
+    });
+
+    // Get value from input field and store it in the passable object
     var postObject = {
       screenName: $('#inputUsername')[0].value
     }
 
     $.ajax({
       type: 'POST',
-      url: this.url + '/name',
+      url: '/name',
       data: JSON.stringify(postObject),
       contentType: 'application/json',
       success: (data) => {
         console.log('POST request: success');
-        console.log(data);
+        // Changes stage to analytics
+        // and sets received information to app state
+        this.setState({
+          analytics: data,
+          stage: 'analytics'
+        });
       },
       error: (err) => {
         console.log('POST request: error', err);
@@ -39,19 +60,23 @@ class App extends React.Component {
 
   render() {
 
-    return (
-      <div>
-        <h1>Welcome to Tweetrics!</h1>
-        <div>
-          <form className='mainForm'>
-            <h3>Enter a username</h3>
-            <input id="inputUsername" type="text" name="username"></input>
-            <button type="submit" onClick={this.handleClick}>SUBMIT</button>
-          </form>
-        </div>
-        <div>
+    // Conditional rendering based on stage of the app
+    let element = '';
+    if (this.state.stage === 'landing') {
+      element = <Landing handleClick={this.handleClick} feed={this.state.feed}/>;
+    }
 
-        </div>
+    if (this.state.stage === 'loading') {
+      element = <Loading />;
+    }
+
+    // if (this.state.stage === 'analytics') {
+    //   element = <Analytics analytics={this.state.analytics}/>;
+    // }
+
+    return (
+      <div className="renderElement">
+      {element}
       </div>
     )
   }
