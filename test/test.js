c73487b6ff9ffd5c1153f6014639ac19691b05c9
Hack Reactor Student <student@hackreactor.com>
2017-08-10 19:50:32 -0700

    added tests

--- test/test.js
+++ test/test.js
@@ -5,34 +5,71 @@ var expect = chai.expect;
 var app = require('../server/server.js');
 
 
- // describe(‘initial’), function() {
- //   it (‘should enter in a username’, function(done) {
+describe("post requests", function () {
 
- //   })
- // }
-
-describe('basic repos', function() {
-  it('it should post properly', function(done) {
+  it ("posts a new user to /name", function(done) {  //app.use(bodydyParser.json());
+  // app.post('/name', function(req, res){
+  //   res.send({'screenName':'realDonaldTrump'});
+  // });
+    //var postObject = {screenName: 'realDonaldTrump'};
     request(app)
-       .get('/name')
-       .end(function(err, res) {
-         expect(res.body).to.be.ok;
-         console.log('res', res.body);
-         done();
-       });
+        .post('/name')
+        .send({screenName: 'realDonaldTrump'})
+        .expect(200)
+        .end(done);
   });
-});
-
-
-describe('basic repos', function() {
-  it('it should send properly', function(done) {
+  
+  it ("should find the screenName of the user /name", function(done) {  //app.use(bodydyParser.json());
     request(app)
-       .get('/name')
-       .send('theRealDonaldTrump')
-       .end(function(err, res) {
-         expect(res.body).to.be.ok;
-         console.log('res', res.body);
-         done();
-       });
+        .post('/name')
+        .send({screenName: 'realDonaldTrump'})
+        .expect(200)
+        .expect(function(res) {
+          expect(res.body.screen_name).to.equal('realDonaldTrump');
+        })
+        .end(done);
+  });  
+  
+  it ("it should find the name of the user", function(done) {
+    request(app)
+        .post('/name')
+        .send({screenName: 'realDonaldTrump'})
+        .expect(200)
+        .expect(function(res) {
+          expect(res.body.name).to.equal('Donald J. Trump');
+        })
+        .end(done);
+  });  
+
+
+  it ('should fail when a twitter username doesnt exist', function(done) {  //app.use(bodydyParser.json());
+    request(app)
+        .post('/name')
+        .send({screenName: 'asdfsadfsadfgosdajfoisdajf'})
+        .expect(400)
+        .end(done);
   });
+
+  it ('it should return an array of friends', function(done) {  //app.use(bodydyParser.json());
+    request(app)
+        .post('/name')
+        .send({screenName: 'realDonaldTrump'})
+        .expect(200)
+        .expect(function(res) {
+          expect(res.body.friends).to.be.an('array');
+        })
+        .end(done);
+  });
+  
+  it ("it should return an array of tweets", function(done) {
+    request(app)
+        .post('/name')
+        .send({screenName: 'realDonaldTrump'})
+        .expect(200)
+        .expect(function(res) {
+          expect(res.body.tweets).to.be.an('array');
+        })
+        .end(done);
+  });  
+  
 });
