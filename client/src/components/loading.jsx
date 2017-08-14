8c65472f311a2079ebae577a115362f72251b1de
Hack Reactor Student <student@hackreactor.com>
2017-08-14 11:19:14 -0700

    Add styles to loading

--- client/src/components/loading.jsx
+++ client/src/components/loading.jsx
@@ -3,7 +3,7 @@ import styles from '../../styles/loading.css';
 
 const Loading = (props) => {
   return (
-    <div className="loadingCard">
+    <div className={styles.loading_card}>
       <div>
         <h1>Loading...</h1>
       </div>
