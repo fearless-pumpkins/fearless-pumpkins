713e2bd0be6d3b0f31e51fa8856a5a815cba117c
Jonathan Kim <jykim16@gmail.com>
2017-08-13 17:34:02 -0700

    test for heroku

--- client/src/components/landing.jsx
+++ client/src/components/landing.jsx
@@ -7,14 +7,14 @@ const Landing = (props) => {
     <div className="app">
 
       <div className="mainForm">
-      <h1>Welcome to Tweetrics!</h1>
+      <h1>Welcome to Tweetrics Version2!</h1>
         <form>
           <h3>Enter a username</h3>
           <input id="inputUsername" type="text" name="username"></input>
           <button type="submit" onClick={props.handleClick}>SUBMIT</button>
         </form>
       </div>
-      
+
       <Feed feed={props.feed}/>
 
     </div>
