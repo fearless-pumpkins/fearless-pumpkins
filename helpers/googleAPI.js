30846798c2a9188ffbf96df7d0a7116240aad967
gchoupeaux <gchoupeaux@gmail.com>
2017-08-14 14:27:49 -0700

    read db

--- helpers/googleAPI.js
+++ helpers/googleAPI.js
@@ -12,9 +12,6 @@ var client = language({
 
 const MAX_ENTITIES = 5;
 
-//input from twitterApi - {screen_name: STRING, name: STRING, imageURL: STRING, tweets: [100 STRING], mentions:[100[[multiple{screen_name: STRING, name: STRING}]]], friends: [100{screen_name: STRING, name: STRING}]
-//output to computeMachine -
-
 // trim the google API result
 var parsedEntities = function(results, content) {
   const MAX_ENTITIES = 100;
@@ -32,7 +29,7 @@ var parsedEntities = function(results, content) {
   return content;
 };
 
-
+// https://cloud.google.com/natural-language/docs/analyzing-entities#language-entities-string-nodejs
 // get the analysis fron the google API
 module.exports.sendToGoogleAPI = (content, callback) => {
 
@@ -55,43 +52,4 @@ module.exports.sendToGoogleAPI = (content, callback) => {
   return promiseSendToGoogleAPI;
 };
 
-//example of using sendToGoogleAPI
-// module.exports.sendToGoogleAPI('hello world. My name is Jonathan. What\'s yours?', (err, data)=>{ console.log(JSON.stringify(data)); } );
-
 fs.writeFileSync(gTokenPath, '');
-
-//// usage example
-//interpreting natural language outputs
-//https://cloud.google.com/natural-language/docs/basics
-// var content = 'Hello, world!';
-// var document = {
-//   content: content,
-//   type: language.v1.types.Document.Type.PLAIN_TEXT
-// };
-//
-// //https://cloud.google.com/natural-language/docs/analyzing-sentiment#language-sentiment-string-nodejs
-// client.analyzeSentiment({document: document})
-//   .then(function(responses) {
-//     var response = responses[0];
-//     console.log('analyzeSentiment response: ', response);
-//   })
-//   .catch(function(err) {
-//     console.error('ERROR: ', err);
-//   });
-//
-// // https://cloud.google.com/natural-language/docs/analyzing-entities#language-entities-string-nodejs
-// client.analyzeEntities({ document: document})
-//   .then((results) => {
-//     const entities = results[0].entities;
-//     console.log('analyzeEntities response:');
-//     entities.forEach((entity) => {
-//       console.log(entity.name);
-//       console.log(` - Type: ${entity.type}, Salience: ${entity.salience}`);
-//       if (entity.metadata && entity.metadata.wikipedia_url) {
-//         console.log(` - Wikipedia URL: ${entity.metadata.wikipedia_url}$`);
-//       }
-//     });
-//   })
-//   .catch((err) => {
-//     console.error('ERROR: ', err);
-//   });
