4053bedd4074c6d9e514035858eed37ee1ad8cdb
Yessengerey Bolatov <Yessengerey@users.noreply.github.com>
2017-08-15 16:06:42 -0700

    Feat/client Add chart and more elaborate implementation of Analytics (#54)
    * (style/doc) Move new font file to a separate fonts folder
    
    * (fix) Update fonts paths in css files
    
    * (style) Save color scheme of twitter, update shadows on cards
    
    * (feat) Add canvas.js library to create charts
    
    * (feat) Include canvas chart library into index.html
    
    * (css) Remove padding from body and change app height to auto
    
    * (css) Adjust min and max heights of feed and form cards for better rendering
    
    * (css) Remove height property from logo
    
    * (feat) Rough implementation of influence doughnut chart
    
    * (css) Change background color
    
    * (client) Add styling to analytics and chart
    
    * (doc) Remove unnecessary examples folder from canvas

--- client/src/components/analytics.jsx
+++ client/src/components/analytics.jsx
@@ -10,17 +10,39 @@ class Analytics extends React.Component {
   }
 
   componentDidMount() {
+    var chart = new CanvasJS.Chart(styles.chartContainer, {
+      height: 600,
+      width: 600,
+  		title:{
+  			text: "Rep/Dem Influence"
+  		},
+  		data: [
+  		{
+  			// Change type to "doughnut", "line", "splineArea", etc.
+  			type: "doughnut",
+  			dataPoints: [
+  				{ label: "Democratic",  y: this.state.data.infographicState.dem.percent  },
+  				{ label: "Republican", y: this.state.data.infographicState.rep.percent  }
+  			]
+  		}
+  		]
+  	});
 
+    chart.render();
   }
 
   render() {
+
+    var profileImageStyle = {
+      backgroundImage: `url(${this.state.data.imageUrl})`
+    };
+
     return (
       <div className={styles.main_card}>
 
         <div className={styles.profile_card}>
 
-          <div className={styles.profile_image}>
-            <img src='/static/resources/default_profile_image.png' />
+          <div className={styles.profile_image} style={profileImageStyle}>
           </div>
 
           <div className={styles.profile_info}>
@@ -32,8 +54,11 @@ class Analytics extends React.Component {
         </div>
 
         <div className={styles.analytics_card}>
+
+          <div id={styles.chartContainer} ></div>
+          <p>infographicState other details</p>
         </div>
-        
+
       </div>
     )
   }
