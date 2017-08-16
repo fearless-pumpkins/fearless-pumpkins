ed6d593e83203e1ebf66cd71c5e2836f31fb00e3
Matt Palamos <mattpalamos@hotmail.com>
2017-08-16 10:46:08 -0700

    removed notes

--- server/server.js
+++ server/server.js
@@ -103,108 +103,6 @@ app.post('/usersSearch', function (req, res) {
 });
 
 
-// this function is meant to be used by the admin not by the user
-// update the dataset with new data, this should be run by a cron scheduler one time per week
-//Republicans = [realDonaldTrump, JohnCornyn, tedcruz, marcorubio, SenateMajLdr, SpeakerRyan, mike_pence, SenJohnMcCain, RandPaul, SenPatRoberts]
-//Democrats = [BarackObama, HillaryClinton, CoryBooker, SenWarren, alfranken, SenSchumer, NancyPelosi, KamalaHarris, SenFeinstein, RepMcNerney]
-// app.post('/datasetUpdate', function (req, res) {
-//   if (!req.body) { return res.sendStatus(400); }
-
-//   // WARNING NOW THE FUNCTION DOOESN'T TAKE AN ARRAY OF PEOPLE
-
-//   // should received a list of screen name and their affliliation (rep or dem)
-//   console.log('POST received screen_names: ', req.body.screenName);
-
-//   // for each
-//   twitterApi.getTweets(req.body.screenName)
-//     .then(function(parsedTweets) {
-//       return parsedTweets;
-
-//     }).then(function(parsedTweets) {
-//       // for each
-//       var parsedTweetsWithFriends = twitterApi.getFriends(parsedTweets);
-//       return parsedTweetsWithFriends;
-
-//     }).then(function(parsedTweetsWithFriends) {
-//       // lexical analysis on all the tweets join from people of the list
-//       var lexicalAnalysisWithFriends = googleApi.sendToGoogleAPI(parsedTweetsWithFriends);
-//       return lexicalAnalysisWithFriends;
-
-//     }).then(function(lexicalAnalysisWithFriends) {
-
-//       var dbOutput = db.writeDataset(lexicalAnalysisWithFriends);
-//       return dbOutput;
-
-//     }).then(function(dbOutput) {
-//       res.status(200).send(dbOutput);
-
-//     }).catch(function(err) {
-//       console.log('error: ', err);
-//       res.status(400).send(err);
-//     });
-// });
-
-// '100_common_words': [[]],
-// '100_tweets_to': [[]],
-// '100_shared_link': [[]],
-// '100_common_friends': [[]]
-
-// should return to the client the data for the infographic
-app.post('/usersSearch', function (req, res) {
-  if (!req.body) { return res.sendStatus(400); }
-
-  console.log('POST user search');
-
-  twitterApi.getUsersSearch(req.body.q)
-    .then(function(users) {
-      res.status(200).send(users);
-
-    }).catch(function(err) {
-      console.log('error: ', err);
-      res.status(400).send(err);
-    });
-});
-
-// "message": "Your credentials do not allow access to this resource",
-//     "code": 220
-
-// // FUNCTION called by Postman for test
-// // people, look for the tweet feed of somebody input by the user
-// app.post('/statuses/user_timeline', function (req, res) {
-//   if (!req.body) { return res.sendStatus(400); }
-
-//   console.log('POST received screen_name: ', req.body.screenName);
-
-//   twitterApi.getTweets(req.body.screenName, function(err, tweets) {
-//     if (!err) {
-//       console.log('tweets reiceved: ', tweets.length);
-//       res.status(200).send(tweets);
-//     } else {
-//       console.log('error: ', err);
-//       res.status(400).send(err);
-//     }
-//   });
-
-// });
-
-// FUNCTION called by Postman for test
-// people, look for the tweet feed of somebody input by the user
-// app.post('/friends/list', function (req, res) {
-//   if (!req.body) { return res.sendStatus(400); }
-
-//   console.log('POST received screen_name: ', req.body.screenName);
-
-//   twitterApi.getFriends(req.body.screenName, function(err, friends) {
-//     if (!err) {
-//       console.log('friend reiceved: ', friends.length);
-//       res.status(200).send(friends);
-//     } else {
-//       console.log('error: ', err);
-//       res.status(400).send(err);
-//     }
-//   });
-
-// });
 
 app.listen(app.get('port'), function(err) {
   if (err) {
@@ -214,3 +112,4 @@ app.listen(app.get('port'), function(err) {
 });
 
 module.exports = app;
+
