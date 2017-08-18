f547d8bdc3b299d19f544796e59e1140b7e10c04
Yessengerey Bolatov <yes.murz@gmail.com>
2017-08-18 13:30:02 -0700

    (feat) Add rendering of top 10 users in the feed

--- client/src/components/feedInfo.jsx
+++ client/src/components/feedInfo.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
+import styles from '../../styles/feedInfo.css';
 
-const FeedInfo = (props) => {
-  return (
-    <div>
-      <p>Some Feed Information</p>
-    </div>
-  );
+class FeedInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topTen: props.topTen
+    }
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (JSON.stringify(this.props) !== JSON.stringify(newProps)) {
+      console.log(newProps);
+      this.setState({
+        topTen: newProps.topTen
+      });
+    }
+  }
+
+  render() {
+    let heading =  '';
+    let body = '';
+
+    if (this.state.topTen.length > 0) {
+      heading =
+        <div className={styles.topic}>
+          <a>Top 10 Most Searched Usernames</a>
+        </div>;
+
+      body =
+        <div className={styles.description}>
+          {this.state.topTen.map((user, id) => (<p key={id}>{user.screen_name}</p>))}
+        </div>;
+    } else {
+      heading =
+        <div className={styles.topic}>
+          <a>LOADING...</a>
+        </div>;
+    }
+
+    return (
+      <div className={styles.feed_info}>
+        {heading}
+        {body}
+      </div>
+    );
+  }
 };
 
 export default FeedInfo;
