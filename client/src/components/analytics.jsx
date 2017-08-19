1248db79021e496b5d23e3561f6c2bae7088f8ea
Jonathan Kim <jykim16@gmail.com>
2017-08-19 15:42:53 -0700

    restting analytics.jsx

--- client/src/components/analytics.jsx
+++ client/src/components/analytics.jsx
@@ -7,27 +7,27 @@ class Analytics extends React.Component {
     super(props);
     this.state = {
       data: props.analytics
-    };
+    }
   }
 
   componentDidMount() {
     var chart = new CanvasJS.Chart(styles.chartContainer, {
       height: 600,
       width: 800,
-      title: {
-        text: `Dem/Rep Influence on ${this.state.data.name}`
-      },
-      data: [
-        {
-        // Change type to "doughnut", "line", "splineArea", etc.
-          type: 'doughnut',
-          dataPoints: [
-            { label: 'Democrat', y: this.state.data.infographicState.dem.percent },
-            { label: 'Republican', y: this.state.data.infographicState.rep.percent }
-          ]
-        }
-      ]
-    });
+  		title:{
+  			text: `Dem/Rep Influence on ${this.state.data.name}`
+  		},
+  		data: [
+  		{
+  			// Change type to "doughnut", "line", "splineArea", etc.
+  			type: "doughnut",
+  			dataPoints: [
+  				{ label: "Democrat",  y: this.state.data.infographicState.dem.percent  },
+  				{ label: "Republican", y: this.state.data.infographicState.rep.percent  }
+  			]
+  		}
+  		]
+  	});
 
     chart.render();
   }
@@ -40,39 +40,34 @@ class Analytics extends React.Component {
 
     return (
       <div className={styles.main_card}>
-        <div className={styles.profile_card}>
+        <div className={styles.profile}>
 
-          <div className={styles.profile_image}>
-            <div style={profileImageStyle}>
+          <div className={styles.profile_card}>
+            <div className={styles.profile_image}>
+              <div style={profileImageStyle}>
+              </div>
+            </div>
+
+            <div className={styles.profile_info}>
+              REAL NAME:
+              <p>{this.state.data.name}</p>
+              DESCRIPTION:
+              <p>{this.state.data.description}</p>
+              LOCATION:
+              <p>{this.state.data.location}</p>
             </div>
           </div>
 
-          <div className={styles.profile_info}>
-            <p>{this.state.data.name}</p>
-            <p>{this.state.data.description}</p>
-            <p>{this.state.data.location}</p>
-          </div>
-
-<<<<<<< HEAD
           <div className={styles.analytics_card}>
             <div id={styles.chartContainer} ></div>
             <BubbleApp usertweets={this.state.data.words}/>
           </div>
-=======
-        </div>
-
-        <div className={styles.analytics_card}>
-
-          <div id={styles.chartContainer} ></div>
-          <BubbleApp usertweets={this.state.data.words}/>
->>>>>>> (feat)v2algorithm
 
         </div>
-
       </div>
-    );
+    )
   }
 
-}
+};
 
 export default Analytics;
