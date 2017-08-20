cc8c485d34380503ab832ed7ed839fa20655604a
Gary <yes.murz@gmail.com>
2017-08-19 18:58:49 -0700

    (ui/ux) Add description for word bubble, add style to analytics and make landing titles font size dynamic

--- client/src/components/analytics.jsx
+++ client/src/components/analytics.jsx
@@ -17,7 +17,7 @@ class Analytics extends React.Component {
       height: 600,
       width: 800,
   		title:{
-  			text: `Dem/Rep Percentage Influence on ${this.state.data.name}`
+  			text: `${this.state.data.name}'s Percentage of Influence from Dem/Rep Party`
   		},
   		data: [
   		{
@@ -65,13 +65,28 @@ class Analytics extends React.Component {
           <div className={styles.analytics_card}>
             <div id={styles.chartContainer} ></div>
             <div className={styles.chart_description}>
-              <div id={styles.chart_description_title}>CHART DESCRIPTION</div>
+              <div className={styles.description_title}>CHART DESCRIPTION</div>
               <p>Based on a <i>lexical analysis of {this.state.data.name}'s tweets</i> and
               an <i>analysis of that Twitter user's friends</i>, it has been determined that
               {this.state.data.name} appears to be <a id={styles.dem_percent}>{parseFloat(this.state.data.infographicState.dem.percent).toFixed(2)}%
               Democrat</a> and <a id={styles.rep_percent}>{parseFloat(this.state.data.infographicState.rep.percent).toFixed(2)}% Republican</a>.</p>
             </div>
             <BubbleApp usertweets={this.state.data.words}/>
+            <div className={styles.word_bubble_description}>
+              <div className={styles.description_title}>WORD BUBBLE DESCRIPTION</div>
+              <p>Each bubble in the chart above represents a word of importance to the twitter user, a word that has
+              been used repeatedly or used in a strong contextual sentiment throughout {this.state.data.name}'s tweets.</p>
+              <p>The size of the bubble is based on the <i>Impact</i> it has on calculating the influence of a corresponding
+              political party on the Twitter user undergoing the analysis.</p>
+              <ul>
+                <li>Salience - shows importance or centrality of an entity to the entire text that has been analysed.
+                It ranges from 0 (less salient) to 1 (very salient).</li>
+                <li>Party Affiliation - which political party does the target word most affiliates with.</li>
+                <li>Impact - measures the impact the target word has on calculating the influence of a corresponding
+                political party on the Twitter user.</li>
+              </ul>
+
+            </div>
           </div>
 
         </div>
